a6ce02421f712cc022b2a0a7f6f2cfffd113b87d
jmrada14 <jmrada14@gmail.com>
2026-01-01 11:30:59 -0600

    Initial Commit

---
+++ limen/limenApp.swift
@@ -0,0 +1,32 @@
+//
+//  limenApp.swift
+//  limen
+//
+//  Created by Juan Manuel Rada Leon on 1/1/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct limenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
